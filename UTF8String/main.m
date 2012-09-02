afd3a1da6a766f8d9058856a8527d1fbee77ef60
Nico Schmidt <nico@savoysoftware.com>
2012-09-02 18:13:52 +0200

    Initial Commit

---
+++ UTF8String/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  UTF8String
+//
+//  Created by Nico Schmidt on 02.09.12.
+//  Copyright (c) 2012 Nico Schmidt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
